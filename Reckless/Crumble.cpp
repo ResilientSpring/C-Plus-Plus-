b50b16ffc7576597eb3e4d0d3a6d498dc68bc613
ted10014 <ted10014@gmail.com>
2020-10-13 02:31:51 +0800

    Committed 2020/10/12

--- Reckless/Crumble.cpp
+++ Reckless/Crumble.cpp
@@ -11,7 +11,7 @@ int main() {
 	int ivar = 100;
 	double dvar = 100.0;
 
-	cout << "Original value of ivar: " << ivar << "\n";
+	cout << "Original value of ivar: " << ivar << "\n";  // Carriage-returned 
 	cout << "Original value of dvar: " << dvar << "\n";
 
 	cout << "\n";

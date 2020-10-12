7f5a121047e70feaf3a3d5eb638bc420ffaec5a3
ted10014 <ted10014@gmail.com>
2020-10-13 02:26:58 +0800

    Committed 2020/10/12

---
+++ Reckless/Crumble.cpp
@@ -0,0 +1,27 @@
+/*
+
+This program illustrates the differences between int and double.
+
+*/
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+	int ivar = 100;
+	double dvar = 100.0;
+
+	cout << "Original value of ivar: " << ivar << "\n";
+	cout << "Original value of dvar: " << dvar << "\n";
+
+	cout << "\n";
+
+	ivar = ivar / 3;
+	dvar = dvar / 3.0;
+
+	cout << "ivar after division: " << ivar << "\n";
+
+	cout << "dvar after division: " << dvar;
+
+	return 0;
+}

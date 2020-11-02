0a30320766f03b4b3c011fa885d3691cf3b0155c
ted10014 <ted10014@gmail.com>
2020-11-02 13:50:23 +0800

    Committed 2020/11/2

--- Feet to Meter Conversions/FtoMTable.cpp
+++ Feet to Meter Conversions/FtoMTable.cpp
@@ -12,4 +12,29 @@ using namespace std;
 
 int main() {
 
+	double f; // holds the length in feet. 
+	double m; // holds the conversion to meters. 
+	int counter;
+
+	counter = 0; // Line counter is initially set to zero.
+
+	for (f = 1.0; f <= 100.0; f++) {
+		m = f / 3.28; // convert to meters 
+		cout << f << " feet is " << m << " meters. \n";
+
+
+		counter++; // Increment the line counter with each loop iteration.
+
+		// every 10th line, print a blank line. 
+		if
+			(counter == 10) {
+			cout << "\n"; // output a blank line
+			counter = 0; // reset the line counter. 
+		}
+
+
+	}
+
+	return 0;
+
 }

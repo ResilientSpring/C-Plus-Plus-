c6dddab6e45f576344e10531f869c79269858d24
ted10014 <ted10014@gmail.com>
2020-11-13 01:08:50 +0800

    Committed 2020/11/12

---
+++ Alphabet/Beacon.cpp
@@ -0,0 +1,14 @@
+// This program displays the alphabet. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char letter;
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		cout << letter;
+
+	return 0;
+}

72fa917bad2fa799b308f8be6e7b035fbdd09ccc
Ceiling_roof <ted10014@hotmail.com>
2021-01-17 03:08:12 +0800

    Committed  for 2021/1/16

---
+++ Array/Merchandise.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	int sample[10];  // This reserves 10 integer elements. 
+	int t;
+
+	// load the array
+	for (t = 0; t < 10; ++t) sample[t] = t;
+
+	// display the array 
+	for (t = 0; t < 10; ++t) cout << "This is sample[" << t << "]: " << sample[t] << "\n";
+
+	return 0;
+
+}

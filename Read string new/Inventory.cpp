169043996ca7331af4e20fe5300cc0862c281fbe
Ceiling_roof <ted10014@hotmail.com>
2021-01-23 15:06:21 +0800

    Committed 2021/1/23

---
+++ Read string new/Inventory.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string str;
+
+	cout << "Enter a string: ";
+
+	getline(cin, str);
+
+	cout << "Your string: " << str;
+
+	return 0;
+}

5c4df48dc1223fd139c88d034447e6c8422f9102
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-03 16:02:09 +0800

    Committed 2021/2/3

--- Pointer 3/Heartening.cpp
+++ Pointer 3/Heartening.cpp
@@ -6,8 +6,11 @@ int main() {
 	int* p;
 	int num;
 
+	// ----- End of declaration statements. 
+
 	p = &num;
 
+	//On the left of an assignment statement, * is an operator standing for the value at the memory address.
 	*p = 100; 
 	
 	cout << num << ' ';

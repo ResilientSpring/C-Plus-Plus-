0951d27dfb529d81890dcb6091989b7f96d7e80d
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-02-11 17:35:32 +0800

    Committed 2021/2/11

---
+++ Pointer for string constant/Vindicate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char* ptr; 
+
+	// ----------- End of declaration. 
+
+	ptr = "Pointers add power to C++. \n"; // ptr is assigned the address of this string constant. 
+
+	cout << ptr;
+
+	return 0;
+}

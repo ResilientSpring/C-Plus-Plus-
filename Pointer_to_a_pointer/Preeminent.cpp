46f5494e867fcccb7577893a059553aa14d21325
Ceiling_roof <ted10014@hotmail.com>
2021-02-15 22:44:09 +0800

    Committed 2021/1/15

--- Pointer_to_a_pointer/Preeminent.cpp
+++ Pointer_to_a_pointer/Preeminent.cpp
@@ -3,18 +3,31 @@ using namespace std;
 
 int main() {
 
+	// Refer to Figure 4-2 Single and multiple indirection, on page 159 of the textbook.
+
 	int x;
-	int *p;  // Normal Pointer, the value of the pointer is the address of a value. 
+	int *p;  // Normal Pointer, the value of the pointer is the address of a value(variable). 
 	int **q; // Pointer to a Pointer contains the address of the second pointer.
 
-	//-----End of the declaration.
+	//-----End of the declaration.     See also: array declaration.
 
 	x = 10; 
-	p = &x;  // Assign p the address of x. 
-	q = &p;  // Assign q the address of p which is a pointer.
+	p = &x;  // & is an operator that returns the memory address of its operand.
+	q = &p;  // Assign q the memory address of p which is a pointer.
 
 	// Prints the value of x.
-	cout << **q; // Returns the value at the memory address of q which is a pointer, and then returns the value at the memory address of p.
+	cout << **q; 
+
+	cout << "\n";
+
+	// * is an operator that returns the value at the memory address of its operand.
+	cout << *q; // Returns the value at the memory address of q, i.e. returns the memory address of p.
+
+	cout << "\n";
+
+	cout << *(*q); // Returns the value at the memory address of p, i.e. returns the memory address of x.
+
+	cout << ", which is identical to " << *(p); 
 
 	return 0;
 }

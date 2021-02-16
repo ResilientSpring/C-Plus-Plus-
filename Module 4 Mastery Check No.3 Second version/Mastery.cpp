46bab575b65fa0b04a1e8a279af9050ca551d106
Ceiling_roof <ted10014@hotmail.com>
2021-02-16 16:39:53 +0800

    Committed 2021/2/16

---
+++ Module 4 Mastery Check No.3 Second version/Mastery.cpp
@@ -0,0 +1,99 @@
+/*
+ Write a program that searches an array of ten integers for duplicate values.
+ Have the program display each dubplicate found.
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+	int input;
+
+	//	int size = 10;
+	//	int storage[size];
+
+	int storage[10];
+
+	cout << "Insert 10 integers and the program will check if there is are duplicate integers. \n";
+
+	for (int i = 0; i < 10; i++)
+	{
+		switch (i) {
+
+		case 0:
+			cout << "Now, insert the first number.\n";
+			cin >> input;
+			storage[0] = input;
+			break;
+		case 1:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[1] = input;
+			break;
+		case 2:
+			cout << "Now insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 3:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 4:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 5:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 6:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 7:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 8:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		case 9:
+			cout << "Now, insert the No." << i + 1 << " number.\n";
+			cin >> input;
+			storage[i] = input;
+			break;
+		}
+
+	}
+
+	int j = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++) {
+
+			if (j == i) continue;
+
+			if (storage[i] == storage[j] && i > j) cout << "Duplicate found: " << storage[i] << "\n";
+		}
+	}
+
+	cout << "The 10 numbers you inserted: ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << storage[i];
+	}
+
+	return 0;
+
+}

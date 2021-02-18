f78de73c085e4505a9046366ac5b6ac1e6216ffa
Ceiling_roof <ted10014@hotmail.com>
2021-02-19 01:31:00 +0800

    Committed 2021/2/19

--- Module 4 Mastery Check No.3 Approach II/Life is about moments.cpp
+++ Module 4 Mastery Check No.3 Approach II/Life is about moments.cpp
@@ -1,6 +1,6 @@
 /*
  Write a program that searches an array of ten integers for duplicate values.
- Have the program display each dubplicate found.
+ Have the program display each duplicate found.
 */
 
 #include <iostream>

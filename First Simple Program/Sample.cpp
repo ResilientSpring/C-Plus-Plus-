29e6663f68900e0785f3ce9bae91871a3ca8097d
Ceiling_roof <ted10014@hotmail.com>
2021-02-22 23:58:41 +0800

    Committed 2021/2/22

---
+++ First Simple Program/Sample.cpp
@@ -0,0 +1,18 @@
+/*
+* 
+* This is a simple C++ program. 
+* Call this file Sample.cpp.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// A C++ program behins at main(). 
+
+int main() {
+
+	cout << "C++ is power programming.";
+
+	return 0;
+}

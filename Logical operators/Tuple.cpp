c16b5f6f1b48c64cfc60b8b735785ab0562d7177
Ceiling_roof <ted10014@hotmail.com>
2021-03-12 14:25:37 +0800

    Committed 2021/3/12

---
+++ Logical operators/Tuple.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	bool X = true;
+	bool Y = false;
+
+	cout << (X && Y) << "\n" ;
+	cout << (X || Y) << "\n"; 
+	cout << (!(X && Y)) << "\n";
+
+	return 0;
+
+}

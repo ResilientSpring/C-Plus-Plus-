062b55865591c072abf1aae07814a4f770310da4
Ceiling_roof <ted10014@hotmail.com>
2021-03-13 18:13:23 +0800

    Committed 2021/3/13

---
+++ Concatenate/Traction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int my_num  = 1 + 2 + 3 + 4 + 5;
+	int my_num2 = 1 + 2 + 3 + 4 + 5;
+
+	cout << my_num << ", " << my_num2;
+	cout << "\n";
+	cout << my_num + my_num2;
+
+	return 0;
+}
